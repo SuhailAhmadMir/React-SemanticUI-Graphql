6c1bfb888d2700c193e7a33c3b14723c01368c5e
Suhail Ahmad <mir.suhail907@yahoo.com>
2020-01-16 11:45:49 +0530

    fixing data issue in home.js

--- myclient/src/pages/Home.js
+++ myclient/src/pages/Home.js
@@ -1,12 +1,30 @@
 import React from "react";
 import { useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
+import { Grid, Image, GridRow, GridColumn } from "semantic-ui-react";
+import PostCard from "../components/PostCard";
 function Home() {
   const { loading, data } = useQuery(FETCH_POSTS_QUERY);
-  if (data) {
-    console.log(data);
-  }
+  console.log(data);
+
   return (
+    // <Grid columns={3}>
+    //   <Grid.Row>
+    //     <h1>Recent Posts</h1>
+    //   </Grid.Row>
+    //   <GridRow>
+    //     {loading ? (
+    //       <h1>Loading posts ...</h1>
+    //     ) : (
+    //       posts &&
+    //       posts.map(post => (
+    //         <GridColumn key={post.id}>
+    //           <PostCard post={post} />
+    //         </GridColumn>
+    //       ))
+    //     )}
+    //   </GridRow>
+    // </Grid>
     <div>
       <h1>Home</h1>
     </div>
